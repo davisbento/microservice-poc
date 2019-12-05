4c96a2c45779a291813233a7e58fb552dddd39a1
davisbento <davisilva4222@gmail.com>
2019-12-05 17:14:35 -0300

    Added logstash

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,6 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ClientsModule, Transport } from '@nestjs/microservices';
+import {
+  utilities as nestWinstonModuleUtilities,
+  WinstonModule,
+} from 'nest-winston';
+import * as winston from 'winston';
 
 @Module({
   imports: [
@@ -16,6 +21,18 @@ import { ClientsModule, Transport } from '@nestjs/microservices';
         },
       },
     ]),
+    WinstonModule.forRoot({
+      transports: [
+        new winston.transports.File({ filename: 'combined.log' }),
+        new winston.transports.Console({
+          level: 'info',
+          format: winston.format.combine(
+            winston.format.timestamp(),
+            nestWinstonModuleUtilities.format.nestLike(),
+          ),
+        }),
+      ],
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
--- api/src/app.service.ts
+++ api/src/app.service.ts
@@ -1,10 +1,12 @@
 import { Injectable, Inject } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
+import { Logger } from 'winston';
 
 @Injectable()
 export class AppService {
   constructor(
     @Inject('EVENT_SERVICE') private readonly clientProxy: ClientProxy,
+    @Inject('winston') private readonly logger: Logger,
   ) {}
 
   async onModuleInit() {
@@ -12,6 +14,7 @@ export class AppService {
   }
 
   publish(name: string) {
+    this.logger.log('info', 'Publicando evento...');
     this.clientProxy.emit('user_created', { name }).toPromise();
     return 'Message sent';
   }
